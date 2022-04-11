efecaf01532c546398aa53542b481487b7709c84
sjm-study <50009276+sjm-study@users.noreply.github.com>
2022-04-11 09:46:17 +0800

    123

--- src/page/Battle/index.js
+++ src/page/Battle/index.js
@@ -1,76 +1,111 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col, Alert } from 'antd'
-import SearchInput from '@/components/SearchInput'
-import { pushRoute } from '@/untils/router'
+import React, { useState, useEffect } from "react";
+import { Row, Col, Alert } from "antd";
+import SearchInput from "@/components/SearchInput";
+import { pushRoute } from "@/untils/router";
 
-
-import styles from './index.less'
+import styles from "./index.less";
 
 function Index() {
-    const [playerOneName, setPlayerOneName] = useState('')
-    const [playerTwoName, setPlayerTwoName] = useState('')
+  const [playerOneName, setPlayerOneName] = useState("");
+  const [playerTwoName, setPlayerTwoName] = useState("");
 
+  const submitData = () => {
+    pushRoute({
+      page: "results",
+      query: {
+        play1: playerOneName,
+        play2: playerTwoName
+      }
+    });
+  };
 
-    const submitData = () => {
-        pushRoute({
-            page: "results",
-            query: {
-                play1: playerOneName,
-                play2: playerTwoName
-            }
-        });
-    }
+  return (
+    <div>
+      <p className={styles.title}>Instructions12</p>
+      <Row style={{ display: "flex", justifyContent: "space-around" }}>
+        <Col className={styles.small} md={8} sm={12} xs={12}>
+          <span style={{ display: "block", fontSize: 18 }}>
+            Enter two Github:
+          </span>
+          <i
+            className="fa fa-users"
+            style={{
+              color: "rgb(255, 191, 116)",
+              backgroundColor: "#eee"
+            }}
+          ></i>
+        </Col>
+        <Col className={styles.small} md={8} sm={12} xs={12}>
+          <span style={{ display: "block", fontSize: 18 }}>Battle:</span>
+          <i
+            className="fa fa-fighter-jet"
+            style={{
+              color: "gray",
+              backgroundColor: "#eee"
+            }}
+          ></i>
+        </Col>
+        <Col className={styles.small} md={8} sm={12} xs={12}>
+          <span style={{ display: "block", fontSize: 18 }}>
+            See the winner:
+          </span>
+          <i
+            className="fa fa-trophy"
+            style={{
+              color: "rgb(244,234,47)",
+              backgroundColor: "#eee"
+            }}
+          ></i>
+        </Col>
+      </Row>
 
+      <p style={{ textAlign: "center", fontSize: 24, fontWeight: "bold" }}>
+        Players
+      </p>
+      <Row style={{ display: "flex", width: "100%" }}>
+        <Col style={{ width: "100%", paddingTop: 15 }} md={12} sm={24}>
+          <p style={{ fontSize: 24, fontWeight: "bold" }}>Player One</p>
+          <SearchInput
+            getSuccessData={data => setPlayerOneName(data)}
+            otherName={playerTwoName}
+          />
+        </Col>
 
+        <Col style={{ width: "100%", paddingTop: 15 }} md={12} sm={24}>
+          <p style={{ fontSize: 24, fontWeight: "bold" }}>Player Two</p>
+          <SearchInput
+            getSuccessData={data => setPlayerTwoName(data)}
+            otherName={playerOneName}
+          />
+        </Col>
+      </Row>
 
-    return (
-        <div>
-            <p className={styles.title} >Instructions</p>
-            <Row style={{ display: 'flex', justifyContent: 'space-around' }}>
-                <Col className={styles.small} md={8} sm={12} xs={12}>
-                    <span style={{ display: 'block', fontSize: 18 }}>Enter two Github:</span>
-                    <i className='fa fa-users' style={{
-                        color: 'rgb(255, 191, 116)', backgroundColor: '#eee'
-                    }}></i>
-                </Col>
-                <Col className={styles.small} md={8} sm={12} xs={12}>
-                    <span style={{ display: 'block', fontSize: 18 }}>Battle:</span>
-                    <i className='fa fa-fighter-jet' style={{
-                        color: 'gray', backgroundColor: '#eee'
-                    }}></i>
-                </Col>
-                <Col className={styles.small} md={8} sm={12} xs={12}>
-                    <span style={{ display: 'block', fontSize: 18 }}>See the winner:</span>
-                    <i className='fa fa-trophy' style={{
-                        color: 'rgb(244,234,47)', backgroundColor: '#eee'
-                    }}></i>
-                </Col>
-            </Row>
-
-            <p style={{ textAlign: 'center', fontSize: 24, fontWeight: 'bold' }}>Players</p>
-            <Row style={{ display: 'flex', width: '100%' }}>
-                <Col style={{ width: '100%', paddingTop: 15 }} md={12} sm={24}>
-                    <p style={{ fontSize: 24, fontWeight: 'bold' }}>Player One</p>
-                    <SearchInput getSuccessData={data => setPlayerOneName(data)} otherName={playerTwoName} />
-
-                </Col>
-
-                <Col style={{ width: '100%', paddingTop: 15 }} md={12} sm={24}>
-                    <p style={{ fontSize: 24, fontWeight: 'bold' }}>Player Two</p>
-                    <SearchInput getSuccessData={data => setPlayerTwoName(data)} otherName={playerOneName} />
-                </Col>
-            </Row>
-
-            <div style={{
-                width: '100%', justifyContent: 'center', marginTop: 50,
-                display: playerOneName.length > 0 && playerTwoName.length > 0 ? 'flex' : 'none',
-            }}>
-                <button style={{ fontSize: 18, width: 200, paddingTop: 10, paddingBottom: 10, textAlign: 'center' }}
-                    onClick={submitData}
-                >Battle</button>
-            </div>
-        </div>
-    )
+      <div
+        style={{
+          width: "100%",
+          justifyContent: "center",
+          marginTop: 50,
+          display:
+            playerOneName.length > 0 && playerTwoName.length > 0
+              ? "flex"
+              : "none"
+        }}
+      >
+        <button
+          style={{
+            fontSize: 18,
+            width: 200,
+            paddingTop: 10,
+            paddingBottom: 10,
+            textAlign: "center"
+          }}
+          onClick={submitData}
+        >
+          Battle
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Index
+export default Index;
